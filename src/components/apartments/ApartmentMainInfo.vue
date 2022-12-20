855ef7fab6f9b869b921ffb13af7be79ca721993
Olena Patlata <elenapat468@gmail.com>
2022-12-20 13:10:30 +0200

    start writing component apartmrnt main info for route apartment

---
+++ src/components/apartments/ApartmentMainInfo.vue
@@ -0,0 +1,69 @@
+<template>
+  <article class="apartment-main-info">
+    <div class="wrapper-left">
+      <div class="apartment-main-info__title">
+        <p class="apartment-main-info__text">{{ apartment.title }}</p>
+        <StarRating :rating="apartment.rating" />
+      </div>
+      <img :src="apartment?.imgUrlBig" alt="" class="apartment-main-info__photo" />
+      <div class="apartment-main-info__descr">{{ apartment?.descr }}</div>
+
+    </div>
+    <div class="wrapper-right">
+      <p>444444444444</p>
+    </div>
+  </article>
+</template>
+
+<script>
+import StarRating from '../shared/StarRating.vue';
+export default {
+  name: 'ApartmentMainInfo',
+  components: {
+    StarRating
+  },
+  props: {
+    apartment: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.apartment-main-info{
+  width: 100%;
+  display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  &__title{
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+    padding-bottom: 20px;
+  }
+  &__photo{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  &__descr{
+    padding-top: 30px;
+    padding-bottom: 24px;
+  }
+}
+.wrapper-left{
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  margin-right: 30px;
+}
+.wrapper-right{
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  background: gray;
+}
+</style>
--- src/pages/ApartmentPage.vue
+++ src/pages/ApartmentPage.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="apartment-page">
     <Container>
-      <h2>apartment</h2>
+      <ApartmentMainInfo :apartment="apartment"/>
     </Container>
 
   </div>
@@ -9,18 +9,32 @@
 
 <script>
 import Container from '../components/shared/Container.vue'
+import apartments from '../components/apartments/apartments.js'
+import ApartmentMainInfo from '@/components/apartments/ApartmentMainInfo.vue'
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'ApartmentPage',
-  components: {
-    Container
+  components:{ApartmentMainInfo, Container},
+  data() {
+    return {
+      apartments: apartments
+    }
   },
-  mounted(){
+  mounted() {
     console.log(this.$route.query.name)
+  },
+  computed: {
+    apartment(){
+      return apartments.find(apartment => apartment.id === this.$route.params.id)
+    },
+
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.apartment-page{
+  padding-top: 60px;
+  padding-bottom: 72px;
+}
 </style>
